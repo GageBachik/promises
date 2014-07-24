8a334172fe26b78fa58625f214874660d3aeb22a
gbachik <gbachik@gmail.com>
2014-07-24 15:55:30 -0600

    completed challeng 4

--- program.js
+++ program.js
@@ -1,15 +1,9 @@
 var q = require('q');
 var defer = q.defer();
 //make a promise
-setTimeout(function(){
-	defer.resolve(console.log);
-	defer.reject(console.log);
-}, 300);
+defer.resolve('SECOND');
 
 var promise = defer.promise;
 // use promise
-promise.then(function(callback){
-	callback('I FIRED');
-}, function(callback){
-	callback('I DID NOT FIRE');
-});
+promise.then(console.log, console.log);
+console.log('FIRST');
